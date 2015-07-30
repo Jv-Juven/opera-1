a7c72d21594865b5bd04595e231c102f212e3ad0
jv-juven <juven-jv@foxmail.com>
2015-07-30 19:14:41 +0800

    所有页面的’分页‘按钮改为右对齐

--- public/src/common/subpages-btn.js
+++ public/src/common/subpages-btn.js
@@ -1,11 +1,15 @@
-$('.pagination').addClass('enlighten-subpage-plugin').children().css({
-  'padding': '5px 10px',
+$('.pagination').css({
+	"padding-top": "30px",
+	"float": "right",
+	"margin-right": "-12px"
+}).addClass('enlighten-subpage-plugin').children().css({
+  'padding': '6px 12px',
   'cursor': 'pointer'
 }).filter('.active').find('span').css({
   'color': '#C80000'
 });
-$('.pagination li:nth-child(1)').text('上一页');
-$('.pagination li:nth-last-child(1)').text('下一页');
+$('.pagination li:nth-child(1)').children().text('上一页');
+$('.pagination li:nth-last-child(1)').children().text('下一页');
 $('.pagination .disabled').css({
 	"color": "#ddd"
 }).siblings().hover(function() {
--- public/src/pages/home/society-show/comedy/comedy.js
+++ public/src/pages/home/society-show/comedy/comedy.js
@@ -1,6 +1,6 @@
-$(".pagination li:nth-child(1)").children().text("上一页").css({
+// $(".pagination li:nth-child(1)").children().text("上一页").css({
 	
-});
-$(".pagination li:nth-last-child(1)").children().text("下一页").css({
+// });
+// $(".pagination li:nth-last-child(1)").children().text("下一页").css({
 	
-});
+// });
