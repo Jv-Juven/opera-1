f65672e15966ecd52d31ae6fc8e797b41423fed1
jv-juven <juven-jv@foxmail.com>
2015-08-03 20:38:42 +0800

    ‘个人中心’-》‘相册’,前后台接口文档的修改

--- public/src/pages/userCenter/information/information.js
+++ public/src/pages/userCenter/information/information.js
@@ -62,11 +62,8 @@ $(".img-char").click(function() {
     
 });
 
-// $.get("/qiniu/getUpToken",{},function(){
 
-// });
-
-uploader({
+window.uploader({
     browse_button: "change_avatar",
     container: "img_char",
     uptoken_url: "/qiniu/getUpToken",
@@ -94,51 +91,7 @@ uploader({
     }
 });
 
-function uploader(options, handlers){
-    // console.log("上传头像");
-    var callback, config, name, uploader;
-    config = $.extend({},{
-        runtimes: 'html5,flash,html4',
-        browse_button: 'click-file',
-        uptoken_url: '/qiniu/getUpToken',
-        domain: "http://7sbxao.com1.z0.glb.clouddn.com/",
-        container: 'container',
-        max_file_size: '5mb',
-        flash_swf_url: '/lib/plupload/Moxie.swf',
-        max_retries: 3,
-        dragdrop: false,
-        drop_element: 'container',
-        chunk_size: '4mb',
-        auto_start: true,
-        unique_names: true,
-        save_key: true,
-        statusTip: '.image-upload-tips',
-        init: {
-          'Error': function(up, err, errTip) {
-            return console.log(errTip);
-          },
-          'BeforeUpload': function(up, file) {
-            return $(this.getOption().statusTip).text('准备上传图片');
-          },
-          'UploadProgress': function(up, file) {
-            return $(this.getOption().statusTip).text('正在上传图片');
-          },
-          'FileUploaded': function(up, file, info) {
-            var domain;
-            info = $.parseJSON(info);
-            return domain = up.getOption('domain');
-          },
-          'UploadComplete': function() {
-            return $(this.getOption().statusTip).text('图片上传成功');
-          }
-        }
-    }, options);
-    for (name in handlers) {
-      callback = handlers[name];
-      config.init[name] = callback;
-    }
-    uploader = Qiniu.uploader(config);
-}
+
 
 
 
--- public/src/pages/userCenter/photo-album/photo-album.js
+++ public/src/pages/userCenter/photo-album/photo-album.js
@@ -1,58 +1,128 @@
 
+//点击“创建相册”按钮
+$(".album-box-add").click(function (){
 
-var albumSwiper = new Swiper(".photo-album-swiper",{
-	direction: "horizontal",
-	// freeMode: true,
-	speed: 400,
-	// initialSlide: index,
-	// slidesPerView: 1,
-	spaceBetween: 100,
-	nextButton: ".swiper-button-next",
-	prevButton: ".swiper-button-prev",
+	$(".album-full-screen,.screen-cover-box").fadeIn(500);
+
 });
 
-$(".album-box").click(function() {
-	var id = $(".album-box").attr("data-id");
-	$.get("/user/picture/",{
-		album_id : id
-	},function(data){
+//输入框字数统计
+$("#album_name_input").on("change input",function(){
 
-		for(var pic in data["pictures"]){
-			console.log(data["pictures"][pic].picture);
-			var url = data["pictures"][pic].picture,
-			    div = $('<div class="swiper-slide"></div>').appendTo($(".photo-album-swiper .swiper-wrapper")),
-			    img = $("<img />",{
-			    	src: url
-			    }).appendTo(div);
+	var str = $(this).val(),
+		length = $(this).val().length,
+		count = $(".box-input-percent");
+
+	count.text(length+"/15");
+	if(length > 15){
+		$(this).val(str.substr(0,14));
+	}
+
+});
+
+
+// 关闭新建相册层
+$(".album-full-screen").click(function (){
+
+	$(".album-full-screen,.screen-cover-box").fadeOut(800);
+
+});
+
+//点击“上传图片”
+$("#box_btn").click(function (){
+
+	var album_name = $("#album_name_input").val();
+	if(album_name.length == 0){
+
+		$("#album_name_input").focus();
+		alert("请输入相册名称");
+		return false;
+
+	}
+
+	$.post("/user/personal/add_album",{
+		album_name: album_name
+	},function (data){
+
+		if(data["errCode"] == 0){
+
+			var url_arr = [];
+
+			window.uploader({
+			    browse_button: "box_btn",
+			    container: "box_btn_container",
+			    uptoken_url: "/qiniu/getUpToken",
+			    domain: "http://7xk6xh.com1.z0.glb.clouddn.com/"
+			},{
+			    FileUploaded: function (up,file,info) {
+			        info = $.parseJSON(info);
+			        domain = up.getOption("domain");
+			        url = domain + info.key;
+			        console.log(url);
+			        // url_arr.push(url);
+
+			    },
+			    UploadComplete: function() {
+	               //队列文件处理完毕后,处理相关的事情
+	               console.log(url_arr.length);
+	               $.post("user/personal/upload_image",{
+		               	img_url: url_arr,
+		               	album_id: data["album_id"]
+	               },function (data){
+	               	if(data["errCode"] == 0){
+	               		alert("图片上传成功");
+	               		window.location.href = window.location.href;
+	               	}
+	               	else{
+	               		alert(data["message"]);
+	               	}
+	               });
+		        }
+			});
+
 		}
-		albumSwiper.update();
-		albumSwiper.slideTo(0,0)
-	},"json");
-	// alert(index);
+		else{
+			alert(data["message"]);
+		}
+	});
 
-	// albumSwiper.slideTo(index,400);
-	$(".album-full-screen").fadeIn();
-	// $(".album-full-screen").css({"z-index":"99"}).transition({"opacity":"1"},300);
-	// $(".photo-album-swiper,.swiper-button-next,.swiper-button-prev").click(function() {
-	// 	return false;
-	// });
 
 });
 
+//点击“编辑”按钮
+$(".album-oprate .album-edit").click(function (){
+	$(this).parents(".album-box").find(".album-content").children(".noedit-name").hide().end().children(".edit-name").show().blur(function (){
+		var album_name = $(this).val(),
+		album_id = $(this).parents(".album-box").attr("data-id");
+		$.post("",{
+			album_name: album_name,
+			album_id: album_id
+		},function (data){
+			if(data["errCode"] == 0){
+				alert("修改相册名字成功");
+				window.location.href = window.location.href;
+			}
+			else{
+				alert(data["message"]);
+			}
+		});
+	});
+});
 
-$(".album-full-screen .video-close").click(function() {
-	// $(".album-full-screen").transition({"opacity":"0"},300,function() {
-	// 	$(this).css({
-	// 		"z-index" : "-99"
-	// 	});
-
-	// 	$(".photo-album-swiper .swiper-wrapper").html("");
-	// });
-
-    $(".album-full-screen").fadeOut(300,function(){
-    	$(".photo-album-swiper .swiper-wrapper").html("");
-    });
-    
+//点击“删除”按钮
+$(".album-oprate .album-del").click(function (){
+	var album_id = $(this).parents(".album-box").attr("data-id");
+	$.post("/user/personal/delete_album",{
+		album_id: album_id
+	},function (data){
+		if(data["errCode"] == 0){
+			alert("相册删除成功");
+			window.location.href = window.location.href;
+		}
+		else{
+			alert(data["message"]);
+		}
+	});
 });
 
 
