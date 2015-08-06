4bddcf977c78bdef83cfacc2d5aa688fde7c4883
unknown <hulinalex@163.com>
2015-08-06 09:53:24 +0800

    更换七牛服务器

--- app/controllers/UploadController.php
+++ app/controllers/UploadController.php
@@ -6,8 +6,8 @@ class UploadController extends \BaseController {
 
 	public function getUpToken()
 	{
-		$accessKey = 'KnOa3BkwBgjUSOm6TqvtYoARkhGM7blkOIq7G9Mg';
-		$secretKey = '0XmYlKCYaushdgA-R9_d87hyvYPsHDKTnVQTW2Q5';
+		$accessKey = 'ftC668Tvh8pxsfr5IEFLguzH4Huh1hRJDwDrswhu';
+		$secretKey = 's6qCdCReUHy-uYjo0d_jc_ZoQjUEHw2cwu55VwXg';
 		$auth = new Auth($accessKey, $secretKey);
 
 		$bucket = 'opera';
